5cfe5621bff0c0c0dd1f7f42de46023debfb8f63
zhujun98 <zhujun981661@gmail.com>
2019-04-22 17:38:29 +0200

    Return a wsgi.DispatcherMiddleware instance

--- dash_live_view/manager.py
+++ dash_live_view/manager.py
@@ -4,6 +4,7 @@ Author: Jun Zhu <zhujun981661@gmail.com>
 import dash
 import flask
 from flask_caching import Cache
+from werkzeug.wsgi import DispatcherMiddleware
 
 
 class Manager:
@@ -34,21 +35,41 @@ class Manager:
     def cache(self):
         return self._cache
 
-    def add_app(self, name):
-        app = dash.Dash(name,
-                        server=self._server,
-                        sharing=True,
-                        url_base_pathname=f"/{name}/")
+    def new_dash_app(self, name, requests_pathname=None):
+        if requests_pathname is None:
+            requests_pathname = name
+
+        # requests_pathname must end with '/'
+        if not requests_pathname:
+            requests_pathname = "/"
+            pathname = "/"  # key used in bookkeeping app instance
+        else:
+            requests_pathname = f"/{requests_pathname}/"
+            pathname = requests_pathname[:-1]  # strip the last "/"
+
+        app = dash.Dash(name, requests_pathname_prefix=requests_pathname)
 
         app.css.config.serve_locally = True
         app.scripts.config.serve_locally = True
         app.config['suppress_callback_exceptions'] = True
 
         # bookkeeping app
-        self.__apps[name] = app
+        self.__apps[pathname] = app
 
         return app
 
-    def run_server(self, port=None, debug=None, host=None):
+    @property
+    def application(self):
+        """Return an application object.
+
+        The application object can be run as a WSGI app like so
+        $ gunicorn wsgi:application
+        or passed as an argument to the Werkzeug development server like so
+        $ from werkzeug.serving import run_simple
+        $ run_simple('localhost', 8050, application)
+        """
         self._cache.clear()
-        self._server.run(port=port, debug=debug, host=host)
+
+        return DispatcherMiddleware(self._server, {
+            pathname: app.server for (pathname, app) in self.__apps.items()
+        })
